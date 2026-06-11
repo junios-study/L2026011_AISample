e4ee8d62cd7d7c6f41040ea9187fe626fe8638e7
junios-study <junios@naver.com>
2026-06-11 14:33:25 +0900

    공격 체크 태스크

---
+++ Source/AISample/Zombie/BTTask_CheckDistance.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_CheckDistance.h"
+#include "AIController.h"
+#include "Zombie.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_CheckDistance::UBTTask_CheckDistance()
+{
+	NodeName = TEXT("CheckDistance");
+}
+
+EBTNodeResult::Type UBTTask_CheckDistance::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AActor* Player = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(TEXT("Player")));
+	AZombie* Zombie = Cast<AZombie>(OwnerComp.GetAIOwner()->GetPawn());
+
+	if (Player && Zombie)
+	{
+		FVector ZombieLocation = OwnerComp.GetAIOwner()->GetPawn()->GetActorLocation();
+		FVector PlayerLocation = Player->GetActorLocation();
+
+		float Distance = FVector::Distance(ZombieLocation, PlayerLocation);
+
+		if (ECondition::LessThen == TargetCondition)
+		{
+			if (TargetDistance > Distance)
+			{
+				OwnerComp.GetBlackboardComponent()->SetValueAsEnum(GetSelectedBlackboardKey(), (uint8)TargetState);
+				Zombie->SetCurrentState(TargetState);
+			}
+
+		}
+		else if (ECondition::GreaterThen == TargetCondition)
+		{
+			if (TargetDistance < Distance)
+			{
+				OwnerComp.GetBlackboardComponent()->SetValueAsEnum(GetSelectedBlackboardKey(), (uint8)TargetState);
+				Zombie->SetCurrentState(TargetState);
+			}
+		}
+	}
+
+	
+
+	return EBTNodeResult::Succeeded;
+}
